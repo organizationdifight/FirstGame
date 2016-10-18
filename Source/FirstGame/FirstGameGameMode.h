82d297c9a92ccf13c2cc8c775af6ba44e4ef430a
git <git@store-max.ru>
2016-10-18 21:18:15 +0300

    Загружаем файлы проекта

---
+++ Source/FirstGame/FirstGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FirstGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTGAME_API AFirstGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
