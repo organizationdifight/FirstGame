6b722522206873ebd78195290d4b9f58bc19e396
difight <greed_nord@mail.ru>
2016-10-24 09:03:32 +0300

    Обновляем проект

---
+++ Source/FirstGame2/FirstGame2GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FirstGame2.h"
+#include "FirstGame2GameMode.h"
+
+
+
+
---
+++ Source/FirstGame2/FirstGame2GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FirstGame2GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTGAME2_API AFirstGame2GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
